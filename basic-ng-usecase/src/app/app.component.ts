3b7fd07de9e064875f657e22fea607636f90e2bb
Rameshwari <m.rameshwari1994@gmail.com>
2023-05-10 16:57:57 +0530

    Angular

---
+++ basic-ng-usecase/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+ title = 'angular-tags';
+  showSecret = false;
+  log: Date[] = [];
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    // this.log.push(this.log.length + 1);
+    this.log.push(new Date());
+  } 
+
+}
+
