5e1e1b487c4687a52fc5349d1df11a4d894031bb
Rameshwari <m.rameshwari1994@gmail.com>
2023-05-10 13:00:24 +0530

    Practice

---
+++ employee-crud/src/main/java/com/cgi/dao/EmployeeDao.java
@@ -0,0 +1,20 @@
+package com.cgi.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.cgi.model.Employee;
+
+public interface EmployeeDao {
+	
+	public Employee createEmployee(Employee employee) throws SQLException;
+
+	public List<Employee> getAllEmployees() throws SQLException;
+	
+	public void deleteEmployeeById(int employeeId) throws SQLException;
+	
+	public void updateEmployee(int employeeId,String firstName,String lastName,String email) throws SQLException;
+	
+	public void findEmployeeByid(int employeeId) throws SQLException;
+
+}
