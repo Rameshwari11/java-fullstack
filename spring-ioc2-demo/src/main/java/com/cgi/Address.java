84e503ef485adb6b68c9012c61feebafbf2e80cb
Rameshwari11 <131978218+Rameshwari11@users.noreply.github.com>
2023-06-23 13:32:51 +0530

    Add files via upload
    workspace

---
+++ spring-ioc2-demo/src/main/java/com/cgi/Address.java
@@ -0,0 +1,17 @@
+package com.cgi;
+
+public class Address {
+	
+	private String addressLine1;
+	private String addressLine2;
+	public Address(String addressLine1, String addressLine2) {
+		super();
+		this.addressLine1 = addressLine1;
+		this.addressLine2 = addressLine2;
+	}
+	@Override
+	public String toString() {
+		return "Address [addressLine1=" + addressLine1 + ", addressLine2=" + addressLine2 + "]";
+	}
+
+}
